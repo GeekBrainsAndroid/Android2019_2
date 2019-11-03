861683115c56272e2be51a32e6339c1834ee87ff
DwarfPorter <mrvlmor@gmail.com>
2019-11-03 21:25:36 +0300

    BroadcastReceiver: Добавление интент фильтра и нотификации

--- Lesson-6/BroadcastReceiver/app/src/main/java/ru/geekbrains/broadcastreceiver/MainActivity.java
+++ Lesson-6/BroadcastReceiver/app/src/main/java/ru/geekbrains/broadcastreceiver/MainActivity.java
@@ -2,7 +2,16 @@ package ru.geekbrains.broadcastreceiver;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +19,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initNotificationChannel();
+    }
+
+    // инициализация канала нотификаций
+    private void initNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+            int importance = NotificationManager.IMPORTANCE_LOW;
+            NotificationChannel channel = new NotificationChannel("2", "name", importance);
+            notificationManager.createNotificationChannel(channel);
+        }
     }
 }
