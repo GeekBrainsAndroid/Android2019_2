27bf8114b7ad01a366725b0056dc7078bd1818d3
DwarfPorter <mrvlmor@gmail.com>
2019-11-03 21:23:06 +0300

    Room: Обработка провайдера данных

--- Lesson-5/Room/app/src/main/java/ru/geekbrains/room/model/Student.java
+++ Lesson-5/Room/app/src/main/java/ru/geekbrains/room/model/Student.java
@@ -11,22 +11,27 @@ import java.util.Date;
 // @Entity - это признак табличного объекта, то есть объект
 // будет сохранятся в базе данных в виде строки
 // indices - указывает на индексы в таблице
-@Entity(indices = {@Index(value = {"first_name", "last_name"})})
+@Entity(indices = {@Index(value = {Student.FIRST_NAME, Student.LAST_NAME})})
 public class Student {
+
+    public final static String ID = "id";
+    public final static String FIRST_NAME = "first_name";
+    public final static String LAST_NAME = "last_name";
 
     // @PrimaryKey - указывает на ключевую запись,
     // autoGenerate = true - автоматическая генерация ключа
     @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = ID)
     public long id;
 
     // Имя студента
     // @ColumnInfo - позволяет задавать параметры колонки в БД
     // name = "first_name" - такое будет имя колонки
-    @ColumnInfo(name = "first_name")
+    @ColumnInfo(name = FIRST_NAME)
     public String firstName;
 
     // Фамилия студента
-    @ColumnInfo(name = "last_name")
+    @ColumnInfo(name = LAST_NAME)
     public String lastName;
 
     public Date dateBirth;
