a03fac27a92a357b3c34cd9a6beba1ca265d9442
DwarfPorter <mrvlmor@gmail.com>
2019-11-03 17:31:55 +0300

    Service: Сигнал завершения сервиса через широковещательное сообщение с результатом

--- Lesson-3/Service/app/src/main/java/ru/geekbrains/service/MainActivity.java
+++ Lesson-3/Service/app/src/main/java/ru/geekbrains/service/MainActivity.java
@@ -4,14 +4,22 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    static final String BROADCAST_ACTION_CALCFINISHED = "ru.geekbrains.service.calculationfinished";
+    private TextView textResult;
+    private EditText editSeconds;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,12 +30,35 @@ public class MainActivity extends AppCompatActivity {
         initNotificationChannel();
     }
 
+    // Для регистрации Broadcast Receiver
+    @Override
+    protected void onStart() {
+        super.onStart();
+        registerReceiver(calculationFinishedReceiver, new IntentFilter(BROADCAST_ACTION_CALCFINISHED));
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        unregisterReceiver(calculationFinishedReceiver);
+    }
+
     private void initView() {
         Button buttonStartService = findViewById(R.id.buttonStartService);
         buttonStartService.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startService(new Intent(MainActivity.this, MainService.class));
+            }
+        });
+
+        textResult = findViewById(R.id.textResult);
+        editSeconds = findViewById(R.id.editSeconds);
+        findViewById(R.id.buttonCalcService).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int seconds = Integer.parseInt(editSeconds.getText().toString());
+                CalculationService.startCalculationService(MainActivity.this, seconds);
             }
         });
     }
@@ -43,5 +74,18 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-
+    // Получатель широковещательного сообщения
+    private BroadcastReceiver calculationFinishedReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            final long result = intent.getLongExtra(CalculationService.EXTRA_RESULT, 0);
+            // Потокобезопасный вывод данных
+            textResult.post(new Runnable() {
+                @Override
+                public void run() {
+                    textResult.setText(Long.toString(result));
+                }
+            });
+        }
+    };
 }
