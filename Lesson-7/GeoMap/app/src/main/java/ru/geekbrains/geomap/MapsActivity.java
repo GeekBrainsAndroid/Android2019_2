b1d74693a9cdcf4b4f73df1a5df78bd7538f1c53
DwarfPorter <mrvlmor@gmail.com>
2019-11-03 21:29:59 +0300

    GeoMap: Получение текущих координат и обработка пермиссий для получения координат

--- Lesson-7/GeoMap/app/src/main/java/ru/geekbrains/geomap/MapsActivity.java
+++ Lesson-7/GeoMap/app/src/main/java/ru/geekbrains/geomap/MapsActivity.java
@@ -1,30 +1,67 @@
 package ru.geekbrains.geomap;
 
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentActivity;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.widget.EditText;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
+    private static final int PERMISSION_REQUEST_CODE = 10;
+
+    private EditText textLatitude;
+    private EditText textLongitude;
+
     private GoogleMap mMap;
+    private Marker currentMarker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_maps);
+        setContentView(R.layout.activity_main);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        initViews();
+        requestPemissions();
     }
 
+    // Инициализация Views
+    private void initViews() {
+        textLatitude = findViewById(R.id.editLat);
+        textLongitude = findViewById(R.id.editLng);
+    }
+
+    // Запрос пермиссий
+    private void requestPemissions() {
+        // Проверим на пермиссии, и если их нет, запросим у пользователя
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED
+                || ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            // запросим координаты
+            requestLocation();
+        } else {
+            // пермиссии нет, будем запрашивать у пользователя
+            requestLocationPermissions();
+        }
+    }
 
     /**
      * Этот метод позволяет управлять картой, когда она станет доступна.
@@ -42,7 +79,87 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         // Add a marker in Sydney and move the camera
         LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        currentMarker = mMap.addMarker(new MarkerOptions().position(sydney).title("Текущая позиция"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+
+    // Запрос координат
+    private void requestLocation() {
+        // Если пермиссии все таки нет - то просто выйдем, приложение не имеет смысла
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED)
+            return;
+        // Получить менеджер геолокаций
+        LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        Criteria criteria = new Criteria();
+        criteria.setAccuracy(Criteria.ACCURACY_COARSE);
+
+        // получим наиболее подходящий провайдер геолокации по критериям
+        // Но можно и самому назначать какой провайдер использовать.
+        // В основном это LocationManager.GPS_PROVIDER или LocationManager.NETWORK_PROVIDER
+        // но может быть и LocationManager.PASSIVE_PROVIDER, это когда координаты уже кто-то недавно получил.
+        String provider = locationManager.getBestProvider(criteria, true);
+        if (provider != null) {
+            // Будем получать геоположение через каждые 10 секунд или каждые 10 метров
+            locationManager.requestLocationUpdates(provider, 10000, 10, new LocationListener() {
+                @Override
+                public void onLocationChanged(Location location) {
+                    double lat = location.getLatitude();// Широта
+                    String latitude = Double.toString(lat);
+                    textLatitude.setText(latitude);
+
+                    double lng = location.getLongitude();// Долгота
+                    String longitude = Double.toString(lng);
+                    textLongitude.setText(longitude);
+
+                    String accuracy = Float.toString(location.getAccuracy());   // Точность
+
+                    // Переместить метку на текущую позицию
+                    LatLng currentPosition = new LatLng(lat, lng);
+                    currentMarker.setPosition(currentPosition);
+                    mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(currentPosition, (float)12));
+                }
+
+                @Override
+                public void onStatusChanged(String provider, int status, Bundle extras) {
+                }
+
+                @Override
+                public void onProviderEnabled(String provider) {
+                }
+
+                @Override
+                public void onProviderDisabled(String provider) {
+                }
+            });
+        }
+    }
+
+    // Запрос пермиссии для геолокации
+    private void requestLocationPermissions() {
+        if (!ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                || !ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_COARSE_LOCATION)) {
+            // Запросим эти две пермиссии у пользователя
+            ActivityCompat.requestPermissions(this,
+                    new String[]{
+                            Manifest.permission.ACCESS_COARSE_LOCATION,
+                            Manifest.permission.ACCESS_FINE_LOCATION
+                    },
+                    PERMISSION_REQUEST_CODE);
+        }
+    }
+
+
+    // Это результат запроса у пользователя пермиссии
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == PERMISSION_REQUEST_CODE) {   // Это та самая пермиссия, что мы запрашивали?
+            if (grantResults.length == 2 &&
+                    (grantResults[0] == PackageManager.PERMISSION_GRANTED || grantResults[1] == PackageManager.PERMISSION_GRANTED)) {
+                // Все препоны пройдены и пермиссия дана
+                // Запросим координаты
+                requestLocation();
+            }
+        }
     }
 }
